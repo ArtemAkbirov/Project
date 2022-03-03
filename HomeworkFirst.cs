2dc44900d93d85a1f78bf20d0d501df727f7ae9a
Artem <artem-acc@mail.ru>
2022-03-03 18:24:03 +0300

    HW1

---
+++ HomeworkFirst.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    internal class HomeworkFirst
+    {
+        public static double Task2(double a, double b)
+        {
+            double result = -((5 * a) + (b * b)) / (b - a);
+            return result;
+        }
+        public static double Task4(double a, double b, double c)
+        {
+            double result = (c - b) / a;
+            return result;
+        }
+        public static double Task5(double y1, double y2, double x1, double x2)
+        {
+            double k = (y1 - y2) / (x1 - x2);
+            double n = y2 - k * x2;
+            string s = "Уравнение Y=" + k + "x" + n;
+            return s;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Project;
+double y1 = Convert.ToDouble(Console.ReadLine());
+double y2 = Convert.ToDouble(Console.ReadLine());
+double x1 = Convert.ToDouble(Console.ReadLine());
+double x2 = Convert.ToDouble(Console.ReadLine());
+double z = HomeworkFirst.Task5(y1, y2, x1, x2);
+Console.WriteLine(z);
