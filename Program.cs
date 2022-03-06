5df0195fe3aaf48f8693da45628b6d6873e0181b
Artem <artem-acc@mail.ru>
2022-03-06 14:21:32 +0300

    HomeworkSecondDone

--- Program.cs
+++ Program.cs
@@ -10,8 +10,36 @@
 //Console.WriteLine(a.Item1);
 //Console.WriteLine(a.Item2);
 
-string? a = Console.ReadLine();
-string? b = Console.ReadLine();
-HomeworkFirst.Task3(ref a, ref b);
-Console.WriteLine(a);
-Console.WriteLine(b);
+//string? a = Console.ReadLine();
+//string? b = Console.ReadLine();
+//HomeworkFirst.Task3(ref a, ref b);
+//Console.WriteLine(a);
+//Console.WriteLine(b);
+
+//Homework2
+
+//int a = Convert.ToInt32(Console.ReadLine());
+//int b = Convert.ToInt32(Console.ReadLine());
+//int c = HomeworkSecond.Task1(a, b);
+//Console.WriteLine(c);
+
+//int x = Convert.ToInt32(Console.ReadLine());
+//int y = Convert.ToInt32(Console.ReadLine());
+//string c = HomeworkSecond.Task2(x, y);
+//Console.WriteLine(c);
+
+//int a = Convert.ToInt32(Console.ReadLine());
+//int b = Convert.ToInt32(Console.ReadLine());
+//int c = Convert.ToInt32(Console.ReadLine());
+//string result = HomeworkSecond.Task3(a, b, c);
+//Console.WriteLine(result);
+
+//string result1 = "";
+//string result2 = "";
+//HomeworkSecond.Task4(ref result1, ref result2);
+//Console.WriteLine(result1);
+//Console.WriteLine(result2);
+
+int num = Convert.ToInt32(Console.ReadLine());
+string c = HomeworkSecond.Task5(num);
+Console.WriteLine(c);
